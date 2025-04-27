d7b501ef83103165567c65fc5673a52afd64f77b
hulyayldz <hulya0898yildiz@gmail.com>
2025-04-27 15:17:07 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Person p = new Person("John", "Doe", 20);
+        System.out.println("Firstname: " + p.getFirstName());
+        System.out.println("Lastname: " + p.getLastName());
+        System.out.println("Age: " + p.getAge());
+        Wall w = new Wall(5, 4);
+        System.out.println("area=" + w.getArea());
+        w.setHeight(-1.5);
+        System.out.println("width=" + w.getWidth());
+        System.out.println("height=" + w.getHeight());
+        System.out.println("area=" + w.getArea());
     }
 }
